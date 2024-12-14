79702d5d06ad1b002d727fef1016b7e5d6cebc3d
Mark Anthony Vale <markavale1@gmail.com>
2024-12-14 15:30:54 +0800

    Update logger module

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,6 +1,10 @@
-import logging
-import os
 from datetime import datetime
+import logging
+import colorlog
+import json
+import tzlocal
+
+__all__ = ["logger", "Logger"]
 
 class Logger:
     _instance = None
@@ -30,3 +34,59 @@ class Logger:
     def get_logger(self):
         """Get the logger instance."""
         return self.logger 
+
+class CustomFormatter(colorlog.ColoredFormatter):
+    def __init__(self):
+        super().__init__(
+            '%(log_color)s%(asctime)s %(levelname)s %(funcName)s: %(message)s',
+            datefmt="%Y-%m-%d %H:%M:%S %z",
+            log_colors={
+                "DEBUG": "cyan",
+                "INFO": "green",
+                "WARNING": "yellow",
+                "ERROR": "red",
+                "CRITICAL": "bold_red",
+            },
+            reset=True,
+            style="%",
+        )
+
+
+class JsonFormatter(logging.Formatter):
+    def format(self, record):
+        # Get the system's local timezone
+        local_timezone = tzlocal.get_localzone()
+        formatted_time = datetime.fromtimestamp(record.created, local_timezone).strftime('%Y-%m-%d %H:%M:%S %z')
+        log_record = {
+            "time": formatted_time,
+            "log.level": record.levelname,
+            "service.function.name": record.funcName,
+            "message": record.getMessage(),
+        }
+        return json.dumps(log_record)
+
+
+def add_coloured_handler(logger, use_json=False):
+    if use_json:
+        formatter = JsonFormatter()
+    else:
+        formatter = CustomFormatter()
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    return logger
+
+
+def setup_custom_logger(name, use_json=False):
+    logger = logging.getLogger(name)
+    logger.handlers = []
+
+    add_coloured_handler(logger, use_json)
+
+    logger.setLevel(logging.DEBUG)
+    logger.propagate = False
+
+    return logger
+
+
+logger: logging.Logger = setup_custom_logger(__name__, use_json=True)
